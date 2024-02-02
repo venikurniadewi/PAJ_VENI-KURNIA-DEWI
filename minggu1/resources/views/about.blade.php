f04cf00320b3c961b2ac843114694207729d5471
PAJ <marzukiveni@gmail.com>
2024-02-02 10:10:10 +0700

    first commit

---
+++ minggu1/resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <body>
+            <h1>Ini Halaman About</h1>
+        </body>
+    </head>
+</html>
---
+++ minggu1/resources/views/servis.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <body>
+            <h1>Ini Halaman Services</h1>
+        </body>
+    </head>
+</html>

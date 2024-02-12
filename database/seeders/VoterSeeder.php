b4daad55c6ad0927dde18b7af28c859824e011bd
venikurniadewi <marzukiveni@gmail.com>
2024-02-12 20:27:13 +0700

    commit kedua

---
+++ database/seeders/VoterSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VoterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        //
+        \App\Models\Voter::factory(10)->create();
+
+         \App\Models\Voter::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'address' => 'Mastrip 163',
+         ]);
+    }
+}
